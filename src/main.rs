16b78f508c5756f68766360f274af2dea2c2e969
Otavio Pace <otaviopp8@gmail.com>
2020-08-15 01:30:49 -0300

    Change window title
    Co-authored-by: Julia Boeira <julia.boeira@nubank.com.br>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ggez;
 use ggez::conf::WindowMode;
 use ggez::event;
-use ggez::{ContextBuilder, GameResult};
+use ggez::{conf::WindowSetup, ContextBuilder, GameResult};
 
 mod ball;
 mod draw;
@@ -13,6 +13,7 @@ use state::{MainState, WINDOW_HEIGHT, WINDOW_WIDTH};
 
 pub fn main() -> GameResult {
     let cb = ContextBuilder::new("Pong", "Alibaba and the 50 Thieves")
+        .window_setup(WindowSetup::default().title("Pong"))
         .window_mode(WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT));
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut MainState::new()?;
